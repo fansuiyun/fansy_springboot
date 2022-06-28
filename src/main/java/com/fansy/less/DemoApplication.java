856336280df270be1062e933297419a8fb4e6718
fansuiyun <fsy202136>
2022-06-28 15:30:15 +0800

    springboot

--- src/main/java/com/fansy/less/DemoApplication.java
+++ src/main/java/com/fansy/less/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.fansy.less;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author fansy
  */
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println(123);
+        new SpringApplication(DemoApplication.class).run(args);
     }
 }
