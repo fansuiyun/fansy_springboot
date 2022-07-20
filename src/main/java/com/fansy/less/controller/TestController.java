46afde8d1c7671f3412ebefd699ab9ad6dd5f6cc
fansuiyun <fsy202136>
2022-07-20 10:56:04 +0800

    33333

--- src/main/java/com/fansy/less/controller/TestController.java
+++ src/main/java/com/fansy/less/controller/TestController.java
@@ -34,7 +34,8 @@ public class TestController {
             user.setName("fansy");
             rabbitTemplate.convertAndSend(Constants.topExchange,Constants.routekey,user);
         }
-        System.out.println("123456");
+        System.out.println(1);
+        System.out.println(2);
         return id;
     }
 
